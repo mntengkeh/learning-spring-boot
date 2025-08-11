c9f8d9ce8c4895db0039788d507d427b677ec93b
maxwell 21 <maxwellntengkeh@gmail.com>
2025-08-11 14:41:58 +0100

    handle multiple beans of same type

---
+++ 1_bean-creation-2/bean-creation-2/src/main/java/com/tasha/App.java
@@ -0,0 +1,15 @@
+package com.tasha;
+
+
+import com.tasha.beans.Vehicle;
+import com.tasha.config.BeanSource;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App
+{
+    public static void main( String[] args ) {
+        var context = new AnnotationConfigApplicationContext(BeanSource.class);
+        var vehicleB = context.getBean("vehicle3", Vehicle.class);
+        System.out.println("Bean vehicle: " + vehicleB.getName());
+    }
+}
